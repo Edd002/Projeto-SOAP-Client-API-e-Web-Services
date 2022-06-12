bf9fb151b9bef3c0f6c399a3ec89fef4ffc5b0d2
Edd002 <42621512+Edd002@users.noreply.github.com>
2022-06-12 16:48:12 -0300

    Initial commit

---
+++ src/main/SOAPClient.java
@@ -0,0 +1,98 @@
+package main;
+
+import java.util.HashMap;
+
+import org.jrichardsz.soapeasier.SimpleHttpSoapClient;
+import org.jrichardsz.soapeasier.core.impl.SoapClasspathMessage;
+
+public class SOAPClient {
+
+	public static void main(String[] args) throws Exception {
+		simpleHttpSoapClientTest();
+		System.out.println();
+		simpleHttpSoapClientWithParametersTest();
+		System.out.println();
+		simpleHttpsSoapClientTest();
+		System.out.println();
+		simpleHttpsSoapClientWithBasicAuthTest();
+	}
+
+	private static void simpleHttpSoapClientTest() throws Exception {
+		SimpleHttpSoapClient client = new SimpleHttpSoapClient();
+
+		client.setServiceUrl("http://www.crcind.com/csp/samples/SOAP.Demo.cls");
+		client.setContentType("text/xml; charset=utf-8");
+		client.setSoapAction("http://tempuri.org/SOAP.Demo.AddInteger");
+
+		client.configure();
+
+		SoapClasspathMessage message = new SoapClasspathMessage();
+		message.setXmlLocation("/resources/soap-message-sample.xml");
+		String response = client.performOperation(message);
+
+		System.out.println(response);
+	}
+
+	private static void simpleHttpSoapClientWithParametersTest() throws Exception {
+		SimpleHttpSoapClient client = new SimpleHttpSoapClient();
+
+		client.setServiceUrl("http://www.crcind.com/csp/samples/SOAP.Demo.cls");
+		client.setContentType("text/xml; charset=utf-8");
+		client.setSoapAction("http://tempuri.org/SOAP.Demo.AddInteger");
+
+		client.configure();
+
+		SoapClasspathMessage message = new SoapClasspathMessage();
+		message.setXmlLocation("/resources/soap-message-sample-with-parameters.xml");
+
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("addend1", 5);
+		parameters.put("addend2", 140);   
+
+		String response = client.performOperation(message, parameters);
+
+		System.out.println(response);
+	}
+
+	private static void simpleHttpsSoapClientTest() throws Exception {
+		SimpleHttpSoapClient client = new SimpleHttpSoapClient();
+
+		client.setServiceUrl("http://www.crcind.com/csp/samples/SOAP.Demo.cls");
+		client.setContentType("text/xml; charset=utf-8");
+		client.setSoapAction("http://tempuri.org/SOAP.Demo.AddInteger");
+
+		client.setSkipCertificateValidation(true);
+		client.setAllowedStringHostnames("crcind.com");
+
+		client.configure();
+
+		SoapClasspathMessage message = new SoapClasspathMessage();
+		message.setXmlLocation("/resources/soap-message-sample.xml");
+		String response = client.performOperation(message);
+
+		System.out.println(response);
+	}
+
+	private static void simpleHttpsSoapClientWithBasicAuthTest() throws Exception {
+		SimpleHttpSoapClient client = new SimpleHttpSoapClient();
+
+		client.setServiceUrl("http://www.crcind.com/csp/samples/SOAP.Demo.cls");
+		client.setContentType("text/xml; charset=utf-8");
+		client.setSoapAction("http://tempuri.org/SOAP.Demo.AddInteger");
+
+		client.setSkipCertificateValidation(true);
+		client.setAllowedStringHostnames("crcind.com");
+
+		client.setBasicAuthUser("jane");
+		client.setBasicAuthPassword("doe");
+
+		client.configure();
+
+		SoapClasspathMessage message = new SoapClasspathMessage();
+		message.setXmlLocation("/resources/soap-message-sample.xml");
+
+		String response = client.performOperation(message);
+
+		System.out.println(response);;
+	}
+}
